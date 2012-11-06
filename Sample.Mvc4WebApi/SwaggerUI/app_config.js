18f370f6da93d22024fb82a928ea572362c476b9
Dan Schlossberg <danschlossberg@gmail.com>
2012-11-06 11:10:43 -0500

    created sample app_config for swaggerUI

---
+++ Sample.Mvc4WebApi/SwaggerUI/app_config.js
@@ -0,0 +1,18 @@
+﻿(function () {
+
+    var config = {
+        consumerKey: "YOUR_CONSUMER_KEY",
+        consumerSecret: "YOUR_CONSUMER_SECRET",
+        requestTokenUrl: "https://API.DOMAIN.COM/v1/.../RequestToken",
+        accessTokenUrl: "https://API.DOMAIN.COM/v1/.../AccessToken",
+        authorizeTokenUrl: "https://API.DOMAIN.COM/v1/.../AuthorizeToken", // not implemented.  returns nil
+        authorize_callback: "",
+        version: "1.0",
+        signatureMethod: "HMAC-SHA1",
+        rootUrl: "https://API.DOMAIN.COM/v1/"
+    };
+
+    window.APP = window.APP || {};
+    window.APP.config = config;
+})();
+
