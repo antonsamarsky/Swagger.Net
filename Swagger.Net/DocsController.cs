ec148eed2ead0c6d1fec28e15c964129a8012372
Dan Schlossberg <danschlossberg@gmail.com>
2012-11-12 22:09:12 -0500

    refactory toward factories (move logic out of controller)

--- Swagger.Net/DocsController.cs
+++ Swagger.Net/DocsController.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Controllers;
 using System.Web.Http.Description;
+using Swagger.Net.Factories;
 
 namespace Swagger.Net
 {
-     [ApiExplorerSettings(IgnoreApi = true)]
+    [ApiExplorerSettings(IgnoreApi = true)]
     public class DocsController : ApiController
     {
         /// <summary>
@@ -17,65 +20,44 @@ namespace Swagger.Net
         /// <returns>JSON document representing structure of API</returns>
         public HttpResponseMessage Get()
         {
-            var docProvider = (XmlCommentDocumentationProvider)GlobalConfiguration.Configuration.Services.GetDocumentationProvider();
-
-            ResourceListing r = SwaggerGen.CreateResourceListing(ControllerContext);
-            List<string> uniqueControllers = new List<string>();
-
-            foreach (var api in GlobalConfiguration.Configuration.Services.GetApiExplorer().ApiDescriptions)
-            {
-                string controllerName = api.ActionDescriptor.ControllerDescriptor.ControllerName;
-                if (uniqueControllers.Contains(controllerName)) continue;
-
-                uniqueControllers.Add(controllerName);
-
-                ResourceApi rApi = SwaggerGen.CreateResourceApi(api);
-                r.apis.Add(rApi);
-            }
-
+            var factory = new ResourceListingFactory();
+            var r = factory.CreateResourceListing(ControllerContext);
             return WrapResponse(r);
         }
 
-
-
         public HttpResponseMessage Get(string id)
         {
-            var rtn = getDocs(id);
-
+            var rtn = getDocs(id, this.Request.RequestUri);
             return WrapResponse(rtn);
         }
 
-        private ResourceListing getDocs(string controllerName)
+        private static ResourceListing getDocs(string controllerName, Uri uri)
         {
             var docProvider = (XmlCommentDocumentationProvider)GlobalConfiguration.Configuration.Services.GetDocumentationProvider();
+            ResourceListing r = SwaggerGen.CreateResourceListing(uri, controllerName, true);
 
-            ResourceListing r = SwaggerGen.CreateResourceListing(this.Request.RequestUri, controllerName, true);
-
-            foreach (var api in GlobalConfiguration.Configuration.Services.GetApiExplorer().ApiDescriptions)
+            foreach (var api in GlobalConfiguration.Configuration.Services.GetApiExplorer().ApiDescriptions
+                                    .Where(api => api.ActionDescriptor.ControllerDescriptor.ControllerName == controllerName))
             {
-                string apiControllerName = api.ActionDescriptor.ControllerDescriptor.ControllerName;
-                if (api.Route.Defaults.ContainsKey(SwaggerGen.SWAGGER) ||
-                    apiControllerName.ToUpper().Equals(SwaggerGen.SWAGGER.ToUpper()))
-                    continue;
-
-                // Make sure we only report the current controller docs
-                if (!apiControllerName.Equals(controllerName))
-                    continue;
-
-                ResourceApi rApi = SwaggerGen.CreateResourceApi(api);
+                var rApi = CreateResourceApi(api, docProvider);
                 r.apis.Add(rApi);
-
-                ResourceApiOperation rApiOperation = SwaggerGen.CreateResourceApiOperation(api, docProvider);
-                rApi.operations.Add(rApiOperation);
-
-                foreach (var param in api.ParameterDescriptions)
-                {
-                    ResourceApiOperationParameter parameter = SwaggerGen.CreateResourceApiOperationParameter(api, param, docProvider);
-                    rApiOperation.parameters.Add(parameter);
-                }
             }
 
             return r;
+        }
+
+        private static ResourceApi CreateResourceApi(ApiDescription api, XmlCommentDocumentationProvider docProvider)
+        {
+            ResourceApi rApi = SwaggerGen.CreateResourceApi(api);
+            ResourceApiOperation rApiOperation = SwaggerGen.CreateResourceApiOperation(api, docProvider);
+            rApi.operations.Add(rApiOperation);
+
+            foreach (var param in api.ParameterDescriptions)
+            {
+                ResourceApiOperationParameter parameter = SwaggerGen.CreateResourceApiOperationParameter(api, param, docProvider);
+                rApiOperation.parameters.Add(parameter);
+            }
+            return rApi;
         }
 
         private HttpResponseMessage WrapResponse<T>(T resourceListing)
---
+++ Swagger.Net/Factories/ResourceListingFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace Swagger.Net.Factories
+{
+    public class ResourceListingFactory
+    {
+        public ResourceListing CreateResourceListing(HttpControllerContext controllerContext)
+        {
+            ResourceListing r = SwaggerGen.CreateResourceListing(controllerContext);
+            List<string> uniqueControllers = new List<string>();
+
+            foreach (var api in GlobalConfiguration.Configuration.Services.GetApiExplorer().ApiDescriptions)
+            {
+                string controllerName = api.ActionDescriptor.ControllerDescriptor.ControllerName;
+                if (uniqueControllers.Contains(controllerName)) continue;
+
+                uniqueControllers.Add(controllerName);
+
+                ResourceApi rApi = SwaggerGen.CreateResourceApi(api);
+                r.apis.Add(rApi);
+            }
+            return r;
+        }
+    }
+}
